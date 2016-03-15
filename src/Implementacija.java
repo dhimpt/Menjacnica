718b5ad9a89264b69f8153ad89802d9381a65536
dhimpt <olk42385@euaqa.com>
2016-03-15 19:11:08 +0100

    Dodata klasa 'Implementacija' koja implementira interfejs 'Menjacnica'.

---
+++ src/Implementacija.java
@@ -0,0 +1,24 @@
+import menjacnica.Kurs;
+import specifikacija.Menjacnica;
+
+public class Implementacija implements Menjacnica {
+
+	@Override
+	public boolean dodajKursValute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean obrisiKursValute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Kurs vratiKursValute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
