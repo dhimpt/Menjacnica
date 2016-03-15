402c3841a3e94c1851b37dd99fba7cd017033cf4
dhimpt <olk42385@euaqa.com>
2016-03-15 20:33:38 +0100

    Implementirane do kraja metode klase 'Implementacija' koja implementira interfejs 'Menjacnica'.

--- src/Implementacija.java
+++ src/Implementacija.java
@@ -1,26 +1,45 @@
+import java.util.LinkedList;
+
 import menjacnica.Kurs;
+import menjacnica.Poslovnica;
+import menjacnica.Valuta;
 import specifikacija.Menjacnica;
 
-public class Implementacija implements Menjacnica {
+public class Implementacija extends Poslovnica implements Menjacnica {
+
+	public Implementacija(LinkedList<Valuta> valute) {
+		super(valute);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public boolean dodajKursValute(String puniNazivValute, String skraceniNazivValute, Kurs kursValute) {
-		// TODO Auto-generated method stub
+		if (puniNazivValute == null || skraceniNazivValute == null)
+			return false;
+		if (getValute().add(new Valuta(puniNazivValute, skraceniNazivValute, kursValute)))
+			return true;
 		return false;
 	}
 
 	@Override
 	public boolean obrisiKursValute(String puniNazivValute, String skraceniNazivValute, Kurs kursValute) {
-		// TODO Auto-generated method stub
+		if (puniNazivValute == null || skraceniNazivValute == null)
+			return false;
+		if (getValute().remove(new Valuta(puniNazivValute, skraceniNazivValute, kursValute)))
+			return true;
 		return false;
 	}
 
 	@Override
 	public Kurs vratiKursValute(String puniNazivValute, String skraceniNazivValute) {
-		// TODO Auto-generated method stub
+		if (puniNazivValute == null || skraceniNazivValute == null)
+			return null;
+		for (int i = 0; i < getValute().size(); i++) {
+			if (getValute().get(i).getPunNaziv().equals(puniNazivValute)
+					&& getValute().get(i).getSkraceniNaziv().equals(skraceniNazivValute))
+				return getValute().get(i).getKursValute();
+		}
 		return null;
 	}
-
-
 
 }
