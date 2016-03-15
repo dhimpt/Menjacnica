35578abe908d56c98fd76e2b584e8c5afb193ec1
dhimpt <olk42385@euaqa.com>
2016-03-15 17:56:19 +0100

    Dodat interfejs Menjacnica. U njega dodate metode: dodajKursValute(), obrisiKursValute() i vratiKursValute().

---
+++ src/specifikacija/Menjacnica.java
@@ -0,0 +1,11 @@
+package specifikacija;
+
+import menjacnica.Kurs;
+
+public interface Menjacnica {
+
+	public boolean dodajKursValute();
+	public boolean obrisiKursValute();
+	public Kurs vratiKursValute();
+	
+}
